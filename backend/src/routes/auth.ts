d43a5050edd2d486c5ef705972bd3c697bdec47c
Sadeepa Bandara <sadeepadexter@gmail.com>
2024-07-14 22:37:26 +0530

    updated authentication

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response } from "express";
-import User from "../models/user";
-import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-// /api/users/register
 router.post(
-  "/register",
+  "/login",
   [
-    check("firstName", "First Name is required").isString(),
-    check("lastName", "Last Name is required").isString(),
     check("email", "Email is required").isEmail(),
     check(
       "password",
@@ -22,20 +20,22 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ message: errors.array() });
     }
-    try {
-      let user = await User.findOne({
-        email: req.body.email,
-      });
 
-      if (user) {
-        return res.status(400).json({ message: "User already exist" });
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        res.status(400).json({ message: "Invalid credentials" });
       }
 
-      user = new User(req.body);
-      user.save();
+      const isMatch = user && (await bcrypt.compare(password, user.password));
+      if (!isMatch) {
+        res.status(400).json({ message: "Invalid credentials" });
+      }
 
       const token = jwt.sign(
-        { userId: user.id },
+        { userId: user?.id },
         process.env.JWT_SECRET_KEY as string,
         {
           expiresIn: "1d",
@@ -48,9 +48,10 @@ router.post(
         maxAge: 86400000,
       });
 
-      return res.sendStatus(200);
+      res.status(200).json({ userId: user?._id });
     } catch (error) {
-      res.status(500).send({ message: "Something went wrong" });
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
     }
   },
 );
