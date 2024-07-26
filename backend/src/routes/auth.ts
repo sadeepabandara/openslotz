8076fcc5efdeb509bf0087228fd2c5a37f900e0f
Sadeepa Bandara <sadeepadexter@gmail.com>
2024-07-26 18:50:03 +0530

    update authentication

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -26,12 +27,12 @@ router.post(
     try {
       const user = await User.findOne({ email });
       if (!user) {
-        res.status(400).json({ message: "Invalid credentials" });
+        return res.status(400).json({ message: "Invalid credentials" });
       }
 
       const isMatch = user && (await bcrypt.compare(password, user.password));
       if (!isMatch) {
-        res.status(400).json({ message: "Invalid credentials" });
+        return res.status(400).json({ message: "Invalid credentials" });
       }
 
       const token = jwt.sign(
@@ -48,12 +49,25 @@ router.post(
         maxAge: 86400000,
       });
 
-      res.status(200).json({ userId: user?._id });
+      return res.status(200).json({ userId: user?._id });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
+      if (!res.headersSent) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
     }
   },
 );
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+  res.status(200).send({ userId: req.userId });
+});
+
+router.post("/logout", (req: Request, res: Response) => {
+  res.cookie("auth_token", "", {
+    expires: new Date(0),
+  });
+  res.send();
+});
 
 export default router;
